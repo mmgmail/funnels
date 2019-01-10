41bf321fcd53de4de3b699ecd95454c524e39d71
mmgmail <qwerty.qq185@gmail.com>
2019-01-10 16:32:50 +0200

    remove build

--- build/js/common.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,c,e){"use strict";e(3);c.a=function(n,c){var e=document.createElement("ul");e.className=c;var i="";return n.forEach(function(n){i+="<li>"+n+"</li>"}),e.innerHTML=i,e}},,,function(n,c){}]]);
