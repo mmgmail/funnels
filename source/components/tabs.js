b6a3855d928af25efca45c8d92072147f61cbb84
mmgmail <qwerty.qq185@gmail.com>
2019-01-12 01:25:16 +0200

    lazy images

---
+++ source/components/tabs.js
@@ -0,0 +1,21 @@
+export default function() {
+  function openTabs(evt, idTab) {
+    var i,l, tabcontent, tablinks;
+
+    tablinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(' active', '');
+    }
+
+    tabcontent = document.getElementsByClassName('tab-pane');
+    for (l = 0; l < tabcontent.length; l++) {
+      tabcontent[l].className = tabcontent[l].className.replace(' active', '');
+    }
+
+    document.getElementById(idTab).className += ' active';
+    evt.currentTarget.className += ' active';
+
+  }
+
+  window.openTabs = openTabs;
+}
--- source/pages/payment-retargeted/payment-retargeted.js
+++ source/pages/payment-retargeted/payment-retargeted.js
@@ -1,22 +1,7 @@
+import lazy from '../../components/lazy';
+import tabs from '../../components/tabs';
 import './index.css';
 import './main.scss';
 
-function openTabs(evt, idTab) {
-  var i,l, tabcontent, tablinks;
-
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
-  }
-
-  tabcontent = document.getElementsByClassName('tab-pane');
-  for (l = 0; l < tabcontent.length; l++) {
-    tabcontent[l].className = tabcontent[l].className.replace(' active', '');
-  }
-
-  document.getElementById(idTab).className += ' active';
-  evt.currentTarget.className += ' active';
-
-}
-
-window.openTabs = openTabs;
+lazy();
+tabs();
